f62eaff3953c828be7246eb3a26193fc2b6e90a6
DichterDev <dichterdev.null@gmail.com>
2026-09-03 16:54:57 +0200

    chore: cleanup imports

--- src/main/java/com/devops/devops2026/rest/Controller.java
+++ src/main/java/com/devops/devops2026/rest/Controller.java
@@ -1,11 +1,6 @@
 package com.devops.devops2026.rest;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController

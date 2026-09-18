4d13c7adc8871ceae9b92e296c67c76d6cad6c5a
DichterDev <dichterdev.null@gmail.com>
2026-09-18 09:48:25 +0200

    feat: added test endpoint

--- src/main/java/com/devops/devops2026/rest/Controller.java
+++ src/main/java/com/devops/devops2026/rest/Controller.java
@@ -1,6 +1,8 @@
 package com.devops.devops2026.rest;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,10 @@ public class Controller {
     @GetMapping("/")
     public String hello() {
         return "Hello!";
+    }
+
+    @GetMapping("/test/{name}")
+    public ResponseEntity<String> test(@PathVariable String name) {
+        return ResponseEntity.ok(name);
     }
 }
